256858adccb413f40dabe5c7543baf52f24d6050
rektide de la faye <rektide@voodoowarez.com>
2017-05-25 00:45:35 -0400

    Fix suffix handling & running.

--- json2es6m.js
+++ json2es6m.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+"use strict"
+
 var
   fs= require( "mz/fs"),
   mrwf= require( "main-routine-with-files")
@@ -7,22 +10,25 @@ async function runFile( filename){
 	  start= 0,
 	  end= filename.length
 	if( !this|| !this.minimist|| !(this.minimist.fullpath|| this.minimist.p)){
-		start= input.filename.lastIndexOf( "/")+ 1
+		start= filename.lastIndexOf( "/")+ 1
 	}
 	if( this&& this.suffix&& filename.endsWith( this.suffix)){
 		end-= this.suffix.length
 	}
 	var
-	  outFilename= filename.substring( start, end),
-	  write= data=> fs.write( outFilename+ ".js", "export default "+ data)
+	  outFilename= filename.substring( start, end)+ ".js",
+	  write= data=> fs.writeFile( outFilename, "export default "+ data)
 	return fs.readFile( filename, {encoding: "utf8"})
 		.then( write)
 }
 
 async function main(opts){
+	process.on( "unhandledRejection", console.error)
+
 	opts= opts|| {}
 	opts.runFile= opts.runFile|| runFile.bind( opts)
 	opts.defaultGlob= opts.defaultGlob|| "*.json"
+	opts.suffix= opts.suffix|| ".json"
 	return mrwf( opts)
 }
 
